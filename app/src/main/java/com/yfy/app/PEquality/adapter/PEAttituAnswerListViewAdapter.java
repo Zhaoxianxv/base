af6cc0649afd32cb9b05c99aa6fad10941cd239d
yfy <1006584058@qq.com>
2020-09-15 17:35:10 +0800

    9.15

---
+++ app/src/main/java/com/yfy/app/PEquality/adapter/PEAttituAnswerListViewAdapter.java
@@ -0,0 +1,67 @@
+package com.yfy.app.PEquality.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckedTextView;
+import com.yfy.app.bean.KeyValue;
+import com.yfy.base.R;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PEAttituAnswerListViewAdapter extends BaseAdapter {
+
+
+    private Context context;
+    private List<KeyValue> datas;
+
+    public void setDatas(List<KeyValue> datas) {
+        this.datas = datas;
+        notifyDataSetChanged();
+    }
+
+    public PEAttituAnswerListViewAdapter(Context context) {
+        this.datas = new ArrayList<>();
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return datas.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return datas.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.public_checked_textview , null);
+            viewHolder.name = convertView.findViewById(android.R.id.text1);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder)convertView.getTag();
+        }
+        viewHolder.name.setText(datas.get(position).getName());
+        viewHolder.name.setChecked(datas.get(position).isIs_selected());
+
+        return convertView;
+    }
+
+    public class ViewHolder{
+        CheckedTextView name;
+    }
+
+
+}
