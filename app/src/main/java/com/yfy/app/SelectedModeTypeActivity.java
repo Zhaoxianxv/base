5c45f5d0152a747aa99b4713e2f242da274c42f8
yfy <1006584058@qq.com>
2020-10-19 14:52:19 +0800

    10.18

---
+++ app/src/main/java/com/yfy/app/SelectedModeTypeActivity.java
@@ -0,0 +1,146 @@
+package com.yfy.app;
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.yfy.app.bean.BaseRes;
+import com.yfy.app.bean.KeyValue;
+import com.yfy.app.net.ResBody;
+import com.yfy.app.net.ResEnv;
+import com.yfy.base.R;
+import com.yfy.base.activity.BaseActivity;
+import com.yfy.final_tag.AppLess;
+import com.yfy.final_tag.data.ColorRgbUtil;
+import com.yfy.final_tag.data.TagFinal;
+import com.yfy.final_tag.recycerview.DefaultItemAnimator;
+import com.yfy.final_tag.recycerview.RecycleViewDivider;
+import com.yfy.final_tag.stringtool.Logger;
+import com.yfy.final_tag.stringtool.StringUtils;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class SelectedModeTypeActivity extends BaseActivity {
+    private static final String TAG = SelectedModeTypeActivity.class.getSimpleName();
+
+    private SelectedModeTypeAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.public_recycler_view);
+        initRecycler();
+        initSQToolbar();
+        setAdapterData();
+    }
+
+
+    private void initSQToolbar() {
+        assert toolbar!=null;
+        toolbar.setTitle("DOME");
+        toolbar.cancelNavi();
+
+
+    }
+
+
+
+
+
+    public RecyclerView recyclerView;
+    public void initRecycler(){
+
+        recyclerView =  findViewById(R.id.public_recycler);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        //添加分割线
+        recyclerView.addItemDecoration(new RecycleViewDivider(
+                mActivity,
+                LinearLayoutManager.HORIZONTAL,
+                1,
+                ColorRgbUtil.getGainsboro()));
+        adapter=new SelectedModeTypeAdapter(this);
+        recyclerView.setAdapter(adapter);
+        adapter.setModeItem(new SelectedModeTypeAdapter.ModeItem() {
+            @Override
+            public void modeItem(String name) {
+                switch (name){
+                    case "视频":
+
+                        break;
+                        default:
+                            break;
+                }
+            }
+        });
+
+    }
+    private List<String> list=StringUtils.listToStringSplitCharacters("视频,一年级2班,一年级3班",",");
+    public List<KeyValue> keyValue_adapter=new ArrayList<>();
+    private void setAdapterData(){
+        keyValue_adapter.clear();
+        for (String s:list){
+            KeyValue one=new KeyValue(TagFinal.TYPE_ITEM);
+            one.setTitle(s);
+            keyValue_adapter.add(one);
+        }
+
+        adapter.setDataList(keyValue_adapter);
+        adapter.setLoadState(TagFinal.LOADING_END);
+    }
+    /**
+     * ----------------------------retrofit-----------------------
+     */
+
+    @Override
+    public void onResponse(Call<ResEnv> call, Response<ResEnv> response) {
+        if (!isActivity())return;
+        dismissProgressDialog();
+        List<String> names=StringUtils.listToStringSplitCharacters(call.request().headers().toString().trim(), "/");
+        String name=names.get(names.size()-1);
+        ResEnv respEnvelope = response.body();
+        if (respEnvelope != null) {
+            ResBody b=respEnvelope.body;
+            if (b.userGetTermListRes !=null){
+                String result=b.userGetTermListRes.result;
+                Logger.e(StringUtils.getTextJoint("%1$s:\n%2$s",name,result));
+                BaseRes res=gson.fromJson(result, BaseRes.class);
+                if (res.getResult().equals("true")){
+                    Logger.e(StringUtils.getTextJoint("%1$s:\n%2$s",name,result));
+                }else{
+                    toastShow("error");
+                }
+            }
+        }else{
+            try {
+                assert response.errorBody()!=null;
+                String s=response.errorBody().string();
+                Logger.e(StringUtils.getTextJoint("%1$s:%2$d:%3$s",name,response.code(),s));
+            } catch (IOException e) {
+                Logger.e("onResponse: IOException");
+                e.printStackTrace();
+            }
+            toastShow(StringUtils.getTextJoint("数据错误:%1$d",response.code()));
+        }
+
+    }
+
+    @Override
+    public void onFailure(Call<ResEnv> call, Throwable t) {
+        if (!isActivity())return;
+        toastShow(R.string.fail_do_not);
+        Logger.e("onFailure  :"+call.request().headers().toString());
+        dismissProgressDialog();
+    }
+
+    @Override
+    public boolean isActivity() {
+        return AppLess.isTopActivy(TAG);
+    }
+}
---
+++ app/src/main/java/com/yfy/app/SelectedModeTypeAdapter.java
@@ -0,0 +1,95 @@
+package com.yfy.app;
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.yfy.app.bean.KeyValue;
+import com.yfy.base.R;
+import com.yfy.final_tag.data.TagFinal;
+import com.yfy.final_tag.recycerview.BaseRecyclerAdapter;
+import com.yfy.final_tag.recycerview.ReViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yfy1 on 2016/10/17.
+ */
+public class SelectedModeTypeAdapter extends BaseRecyclerAdapter {
+
+
+
+    private List<KeyValue> dataList;
+
+    public void setDataList(List<KeyValue> dataList) {
+        this.dataList = dataList;
+    }
+
+
+    SelectedModeTypeAdapter(Activity context) {
+        super(context);
+        this.dataList = new ArrayList<>();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        // 最后一个item设置为FooterView
+        return TagFinal.TYPE_ITEM;
+    }
+
+    @Override
+    public ReViewHolder initViewHolder(ViewGroup parent, int viewType) {
+        //进行判断显示类型，来创建返回不同的View
+        if (viewType == TagFinal.TYPE_ITEM) {
+            return new SelectedClassH(inflater.inflate(R.layout.selected_singe_item_layout, parent, false));
+        }
+        return new ErrorHolder(parent);
+    }
+
+    @Override
+    public void bindHolder(ReViewHolder holder, int position) {
+
+        if (holder instanceof SelectedClassH) {
+            SelectedClassH selectedTermH = (SelectedClassH) holder;
+            selectedTermH.bean=dataList.get(position);
+            selectedTermH.name.setText(selectedTermH.bean.getTitle());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    private class SelectedClassH extends ReViewHolder {
+        TextView name;
+        TextView type;
+        KeyValue bean;
+        SelectedClassH(View itemView) {
+            super(itemView);
+            name=  itemView.findViewById(R.id.selected_item_name);
+            type=  itemView.findViewById(R.id.selected_item_type);
+            name.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (modeItem!=null){
+                        modeItem.modeItem(bean.getName());
+                    }
+
+                }
+            });
+        }
+    }
+
+
+
+    public interface ModeItem{
+        void modeItem(String name);
+    }
+
+    private ModeItem modeItem;
+
+    public void setModeItem(ModeItem modeItem) {
+        this.modeItem = modeItem;
+    }
+}
