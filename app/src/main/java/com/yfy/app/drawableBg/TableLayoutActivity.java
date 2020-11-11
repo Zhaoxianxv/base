df818f5768d34d3927cffe4cc9a4d041d86569e8
zxx <1006584058@qq.com>
2020-11-11 22:31:52 +0800

    home.11.11

--- app/src/main/java/com/yfy/app/drawableBg/TableLayoutActivity.java
+++ app/src/main/java/com/yfy/app/drawableBg/TableLayoutActivity.java
@@ -4,6 +4,13 @@ import android.os.Bundle;
 
 import com.yfy.base.R;
 import com.yfy.base.activity.BaseActivity;
+import com.yfy.final_tag.data.MathTool;
+import com.yfy.final_tag.stringtool.Logger;
+import com.yfy.final_tag.stringtool.StringUtils;
+
+import java.util.List;
+
+import butterknife.OnClick;
 
 
 public class TableLayoutActivity extends BaseActivity {
@@ -12,5 +19,15 @@ public class TableLayoutActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawable_table_layout);
+    }
+
+    @OnClick(R.id.p_e_standard_title)
+    void setOn(){
+        List<String> yearlist=StringUtils.listToStringSplitCharacters("9月,10月,11月,12月,8月",",");
+        List<String> list=MathTool.randomLIstAtList(yearlist,3,2);
+        Logger.e(String.valueOf(list.size())+"个");
+//        for (String s:list){
+////            Logger.e(s);
+//        }
     }
 }
--- app/src/main/java/com/yfy/final_tag/data/MathTool.java
+++ app/src/main/java/com/yfy/final_tag/data/MathTool.java
@@ -1,12 +1,16 @@
 package com.yfy.final_tag.data;
 
 
-import java.text.DecimalFormat;
+import com.yfy.final_tag.stringtool.Logger;
+import com.yfy.final_tag.stringtool.StringJudge;
 
-/**
- * ---------数学函数----------
- */
-public class MathTool {
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+
+
 
 // Collections.sort(list, new Comparator<IllGroup>() {
 //            @Override
@@ -22,8 +26,124 @@ public class MathTool {
 //            }
 //        });
 
+/**
+ * ---------数学函数----------
+ */
+public class MathTool {
+
+    //Math.ceil(): 天花板的意思，就是逢余进一
+    //Math.floor() : 地板的意思，就是逢余舍一
+
+    //System.out.println(Math.sqrt(16)); // 4.0 计算平方根
+    //System.out.println(Math.cbrt(8)); // 2.0 计算立方根
+    //System.out.println(Math.pow(3, 2)); // 9.0 计算a的b次方
+    //System.out.println(Math.max(2.3, 4.5));// 4.5 计算最大值
+    //System.out.println(Math.min(2.3, 4.5));// 2.3 计算最小值
+    //System.out.println(Math.abs(-10.4)); // 10.4 取绝对值
+    //System.out.println(Math.abs(10.1)); // 10.1 取绝对值
+
+    //返回[min_num,max_num+min_num)区间的数
+    //System.out.println(Math.random() * 2 + 1);// [1, 3)的double类型的数
+    public static double getRandom(int min_num,int max_num){
+        return Math.random() * max_num+min_num;
+    }
+    //[min_num, min+max)
+    public static float getRandomInt(int min_num,int max_num){
+        return getRound(Math.random() * max_num+min_num) ;
+    }
 
 
+
+    /**
+     * round 四舍五入，float时返回int值，double时返回long值
+     *  rint 四舍五入，返回double值 注意.5的时候会取偶数 异常的尴尬
+     */
+    //System.out.println(Math.round(10.1)); // 10；System.out.println(Math.round(10.5)); // 11
+    //System.out.println(Math.round(-10.5)); // -10；System.out.println(Math.round(-10.51)); // -11
+    //System.out.println(Math.rint(10.1)); // 10.0；System.out.println(Math.rint(10.7)); // 11.0
+    //System.out.println(Math.rint(11.5)); // 12.0；System.out.println(Math.rint(10.5)); // 10.0
+    //System.out.println(Math.rint(10.51)); // 11.0；System.out.println(Math.rint(-10.5)); // -10.0
+    //System.out.println(Math.rint(-11.5)); // -12.0；System.out.println(Math.rint(-10.51)); // -11.0
+    //System.out.println(Math.rint(-10.6)); // -11.0；System.out.println(Math.rint(-10.2)); // -10.0
+    public static long getRound(double num){
+        return Math.round(num);
+    }
+    public static double getRound(float num){
+        return Math.round(num);
+    }
+
+
+    public static double getRint(double num){
+        return Math.rint(num);
+    }
+
+    /**
+     * ceil天花板的意思，就是逢余进一
+     *  floor地板的意思，就是逢余舍一
+     */
+    //System.out.println(Math.ceil(-10.1)); // -10.0；System.out.println(Math.ceil(10.7)); // 11.0
+    //System.out.println(Math.ceil(-0.7)); // -0.0；System.out.println(Math.ceil(0.0)); // 0.0
+    //System.out.println(Math.ceil(-0.0)); // -0.0；System.out.println(Math.ceil(-1.7)); // -1.0
+
+    //System.out.println(Math.floor(-10.1)); // -11.0；System.out.println(Math.floor(10.7)); // 10.0
+    //System.out.println(Math.floor(-0.7)); // -1.0；System.out.println(Math.floor(0.0)); // 0.0
+    //System.out.println(Math.floor(-0.0)); // -0.0
+
+    public static String stringToGetTwoToDecimals(float f){
+        //构造方法的字符格式这里如果小数不足2位,会以0补足.
+        DecimalFormat decimalFormat=new DecimalFormat(".00");
+        //format 返回的是字符串
+        return decimalFormat.format(f);
+    }
+
+
+
+    /** 在 list数据中随机选出 [min,max]范围个数的 list
+     *
+     *
+     */
+
+    //[min_num, min+max)
+    public static int randomIntMinMax(int min_num,int max_num){
+        String str=String.valueOf(getRound(Math.random() * max_num+min_num));
+
+        return stringToInt(str);
+    }
+    public static int stringToInt(String str){
+        int idx=str.lastIndexOf(".");//注意不能用转义符
+        if (idx==-1){
+            return Integer.valueOf(str);
+        }else{
+            String strNum=str.substring(0,idx);
+            return Integer.valueOf(strNum);
+        }
+    }
+
+    public static List<String> randomLIstAtList(List<String> list,int min,int max){
+        List<String> new_list=new ArrayList<>();
+        if (StringJudge.isEmpty(list))return new_list;
+        int num=randomIntMinMax(min,max);
+        Logger.e(String.valueOf(num)+"分");
+
+        List<String> stringList=new ArrayList<>(list);
+        for (int i=0;i<Math.min(num,list.size());i++){
+            String str=String.valueOf(Math.random() * stringList.size());
+
+            int position=stringToInt(str);
+            new_list.add(stringList.get(position));
+            stringList.remove(position);
+        }
+        return new_list;
+    }
+
+    public static String randomStringAtList(List<String> list){
+        if (StringJudge.isEmpty(list))return "not string ***";
+        String str=String.valueOf(Math.random() * list.size());
+        int idx=str.lastIndexOf(Pattern.quote("."));
+        String strNum=str.substring(0,idx);
+        int num=Integer.valueOf(strNum);
+        return list.get(num);
+    }
 
     public static boolean isLastRowToFirstColumn(int all_num, int position, int column){
 
@@ -45,99 +165,4 @@ public class MathTool {
 
 
 
-    //Math.sqrt() : 计算平方根
-    //Math.cbrt() : 计算立方根
-    //Math.pow(a, b) : 计算a的b次方
-    //Math.max( , ) : 计算最大值
-    //Math.min( , ) : 计算最小值
-    //Math.abs() : 取绝对值
-    //Math.ceil(): 天花板的意思，就是逢余进一
-    //Math.floor() : 地板的意思，就是逢余舍一
-    //Math.rint(): 四舍五入，返回double值。注意.5的时候会取偶数
-    //Math.round(): 四舍五入，float时返回int值，double时返回long值
-    //Math.random(): 取得一个[0, 1)范围内的随机数
-
-
-
-    //System.out.println(Math.sqrt(16)); // 4.0
-    //System.out.println(Math.cbrt(8)); // 2.0
-    //System.out.println(Math.pow(3, 2)); // 9.0
-    //System.out.println(Math.max(2.3, 4.5));// 4.5
-    //System.out.println(Math.min(2.3, 4.5));// 2.3
-    //
-    ///**
-    // * abs求绝对值
-    // */
-    //System.out.println(Math.abs(-10.4)); // 10.4
-    //System.out.println(Math.abs(10.1)); // 10.1
-
-
-    //返回[min_num,max_num+min_num)区间的数
-    //System.out.println(Math.random() * 2 + 1);// [1, 3)的double类型的数
-    public static double getRandom(int min_num,int max_num){
-        return Math.random() * max_num+min_num;
-    }
-    public static float getRandomInt(int min_num,int max_num){
-        return getRound(Math.random() * max_num+min_num) ;
-    }
-
-    /**
-     * round 四舍五入，float时返回int值，double时返回long值
-     */
-    //System.out.println(Math.round(10.1)); // 10
-    //System.out.println(Math.round(10.5)); // 11
-    //System.out.println(Math.round(-10.5)); // -10
-    //System.out.println(Math.round(-10.51)); // -11
-    /**
-     * rint 四舍五入，返回double值 注意.5的时候会取偶数 异常的尴尬
-     */
-    //System.out.println(Math.rint(10.1)); // 10.0
-    //System.out.println(Math.rint(10.7)); // 11.0
-    //System.out.println(Math.rint(11.5)); // 12.0
-    //System.out.println(Math.rint(10.5)); // 10.0
-    //System.out.println(Math.rint(10.51)); // 11.0
-    //System.out.println(Math.rint(-10.5)); // -10.0
-    //System.out.println(Math.rint(-11.5)); // -12.0
-    //System.out.println(Math.rint(-10.51)); // -11.0
-    //System.out.println(Math.rint(-10.6)); // -11.0
-    //System.out.println(Math.rint(-10.2)); // -10.0
-    public static long getRound(double num){
-        return Math.round(num);
-    }
-    public static double getRound(float num){
-        return Math.round(num);
-    }
-
-
-    public static double getRint(double num){
-        return Math.rint(num);
-    }
-
-    /**
-     * ceil天花板的意思，就是逢余进一
-     */
-    //System.out.println(Math.ceil(-10.1)); // -10.0
-    //System.out.println(Math.ceil(10.7)); // 11.0
-    //System.out.println(Math.ceil(-0.7)); // -0.0
-    //System.out.println(Math.ceil(0.0)); // 0.0
-    //System.out.println(Math.ceil(-0.0)); // -0.0
-    //System.out.println(Math.ceil(-1.7)); // -1.0
-    //
-    //System.out.println("-------------------");
-    //
-    /**
-     * floor地板的意思，就是逢余舍一
-     */
-    //System.out.println(Math.floor(-10.1)); // -11.0
-    //System.out.println(Math.floor(10.7)); // 10.0
-    //System.out.println(Math.floor(-0.7)); // -1.0
-    //System.out.println(Math.floor(0.0)); // 0.0
-    //System.out.println(Math.floor(-0.0)); // -0.0
-
-    public static String stringToGetTwoToDecimals(float f){
-        //构造方法的字符格式这里如果小数不足2位,会以0补足.
-        DecimalFormat decimalFormat=new DecimalFormat(".00");
-        //format 返回的是字符串
-        return decimalFormat.format(f);
-    }
 }
